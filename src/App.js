55b1f4280b06126118312bf5edc0935820b0c5f0
“anildalar” <dollorinfotech@gmail.com>
2022-04-22 10:00:44 +0530

    redux tolkit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { addFunds,widthdrawFunds } from '.';
 
 function App() {
+  //1. State/Hook Area
+  let store = useSelector(store=>store);
+  let dispatch = useDispatch()
+
+  //2. Function Defination Area
+  let addFund = ()=>{
+    //alert('Add Fund');
+    let x = parseInt(prompt('Enter the amount to add fund ?'));
+   // alert(x);
+    dispatch(addFunds(x));
+
+  }
+
+  let withdrawFund = ()=>{
+    //alert('okokokokokk,');
+    let y = parseInt(prompt('Enter the amount to withdraw ?'));
+    //alert(y);
+    dispatch(widthdrawFunds(y));
+  }
+  //3. Return Statement
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         {console.log(store.rootReducer.value)}
+          <h1>Redux Toolkit Assignment</h1>
+          <h1>Amount {store.rootReducer.value}</h1>
+          <button onClick={(e)=>{ addFund() }}>Add Fund</button>
+          <button onClick={(e)=>{ withdrawFund()}}>Withdraw</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 
+
+
+
+let reducerSlice = createSlice({
+  name:'myslice',
+  initialState:{
+    value:999
+  },
+  reducers:{
+    //1. P:V
+
+    //2. Method
+    addFunds:function(state, action){
+        console.log(state);
+        console.log(action);
+        console.log(action.payload);
+        state.value += action.payload;
+    },
+    widthdrawFunds:function(state, action){
+      console.log(state);
+        console.log(action);
+        console.log(action.payload);
+        state.value -= action.payload;
+    }
+  }
+});
+
+// Destructore the Object
+export const {addFunds,widthdrawFunds} = reducerSlice.actions
+
+let rootReducerFunction = reducerSlice.reducer  
+let store = configureStore({
+  //1. P:V
+  reducer:{
+    //1. P:V
+    
+    //2. Method
+    rootReducer:rootReducerFunction
+  }
+  //2. Method
+  
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+   
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
